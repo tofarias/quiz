7ee5b37525cbf698b4ca78fd6b464e4325e75a8c
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-14 08:44:43 -0300

    add series

---
+++ quiz/database/series.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    [
+        'grupo' => 'House of Cards',
+        'id' => 1,
+        'respostas' => [
+            'Acorda cedo e come frutas cortadas metodicamente.',
+            'Ela vai atrapalhar seu horário. Oculte o corpo.',
+            'Convence parte das pessoas a esperarem o próximo.',
+            'Fala sobre a política, eleições, como tudo é um absurdo.',
+            'Vou chamar aqui o meu Uber.'
+        ]
+    ],
+    [
+        'grupo' => 'Game of Thrones',
+        'id' => 2,
+        'respostas' => [
+            'Sai da cama com o despertador e se prepara para a batalha da semana',
+            'Levanta a senhora e jura protegê-la com sua vida.',
+            'Ignora as pessoas no elevador e entra de qualquer forma.',
+            'Larga uma frase polêmica e vê uma pequena guerra se formar.',
+            'Pegarei o bus junto com o resto do povo.'
+        ]
+    ],
+    [
+        'grupo' => 'Lost',
+        'id' => 3,
+        'respostas' => [
+            'Só consegue lembrar do seu nome depois do café',
+            'Ajuda-a, mas questiona sua real identidade.',
+            'Você questiona a realidade, as coisas e tudo mais. Sobe de escada.',
+            'Puxa um assunto e te lembram que já foi discutido semana passada.',
+            'No ponto de ônibus mais uma vez, espero não errar a linha de novo.'
+        ]
+    ],
+    [
+        'grupo' => 'Breaking Bad',
+        'id' => 4,
+        'respostas' => [
+            'Levanta e faz café pra todos da casa.',
+            'Oferece para caminharem juntos até um destino em comum.',
+            'Com uma leve intimidação passivo-agressiva, encontra um lugar no elevador.',
+            'Sugere que os colegas trabalhem na ideia de um novo projeto.',
+            'Vou de carro, mas ofereço uma carona para os colegas.'
+        ]
+    ],
+    [
+        'grupo' => 'Silicon Valley',
+        'id' => 5,
+        'respostas' => [
+            'Passa o café e conserta um erro no HTML.',
+            'Testa se ela roda bem no Firefox. Não roda.',
+            'Cria um app que mostra a lotação do elevador. Vende o app e fica milionário.',
+            'Desabafa sobre como odeia PHP. Todo mundo na sala adora PHP.',
+            'Acho que descobri uma forma de fazer aquela senhora rodar no Firefox.'
+        ]
+    ],
+];
