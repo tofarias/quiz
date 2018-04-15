43a9047c01e0faf80a28573e9cfafa1f1f76b2b7
Tiago Farias <tiago.farias.poa@gmail.com>
2018-04-15 12:49:49 -0300

    add frases

--- quiz/database/series.php
+++ quiz/database/series.php
@@ -4,6 +4,7 @@ return [
     [
         'nome' => 'House of Cards',
         'id' => 1,
+        'frase' => 'Você é House of Cards: ataca o problema com método e faz de tudo para resolver a situação',
         'alternativas' => [
             'Acorda cedo e come frutas cortadas metodicamente.',
             'Ela vai atrapalhar seu horário. Oculte o corpo.',
@@ -15,6 +16,7 @@ return [
     [
         'nome' => 'Game of Thrones',
         'id' => 2,
+        'frase' => 'Você é Game of Thrones: não tem muita delicadeza nas ações, mas resolve o problema de forma prática',
         'alternativas' => [
             'Sai da cama com o despertador e se prepara para a batalha da semana',
             'Levanta a senhora e jura protegê-la com sua vida.',
@@ -26,6 +28,7 @@ return [
     [
         'nome' => 'Lost',
         'id' => 3,
+        'frase' => 'Você é Lost: faz as coisas sem ter total certeza se é o caminho certo ou se faz sentido, mas no final dá tudo certo.',
         'alternativas' => [
             'Só consegue lembrar do seu nome depois do café',
             'Ajuda-a, mas questiona sua real identidade.',
@@ -37,6 +40,7 @@ return [
     [
         'nome' => 'Breaking Bad',
         'id' => 4,
+        'frase' => 'Você é Breaking Bad: pra fazer acontecer você toma a liderança, mas sempre contando com seus parceiros.',
         'alternativas' => [
             'Levanta e faz café pra todos da casa.',
             'Oferece para caminharem juntos até um destino em comum.',
@@ -48,6 +52,7 @@ return [
     [
         'nome' => 'Silicon Valley',
         'id' => 5,
+        'frase' => 'Você é Silicon Valley: vive a tecnologia o tempo todo e faz disso um mantra para cada situação no dia.',
         'alternativas' => [
             'Passa o café e conserta um erro no HTML.',
             'Testa se ela roda bem no Firefox. Não roda.',
